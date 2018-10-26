07480feee67862f9e789da360f2aedc38c25dbed
Jeason <Jeason@172.20.10.3>
2018-10-26 14:26:37 +0800

    modify.Jeason

--- src/com/bplead/cad/bean/client/CaxaTemporary.java
+++ src/com/bplead/cad/bean/client/CaxaTemporary.java
@@ -6,31 +6,21 @@ public class CaxaTemporary implements ClientTemporary {
 
 	private static final long serialVersionUID = -2920420379604912729L;
 	private String cache;
-	private String location;
 
 	public CaxaTemporary() {
 
 	}
 
-	public CaxaTemporary(String cache, String location) {
+	public CaxaTemporary(String cache) {
 		this.cache = cache;
-		this.location = location;
 	}
 
 	public String getCache() {
 		return cache;
 	}
 
-	public String getLocation() {
-		return location;
-	}
-
 	public void setCache(String cache) {
 		this.cache = cache;
-	}
-
-	public void setLocation(String location) {
-		this.location = location;
 	}
 
 	@Override
@@ -38,8 +28,6 @@ public class CaxaTemporary implements ClientTemporary {
 		StringBuilder builder = new StringBuilder();
 		builder.append("CaxaTemporary [cache=");
 		builder.append(cache);
-		builder.append(", location=");
-		builder.append(location);
 		builder.append("]");
 		return builder.toString();
 	}
